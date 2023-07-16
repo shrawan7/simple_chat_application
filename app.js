66858d1a425363563be1239dd7dfa953ae02e80e
Shrawan Kumar <53372247+shrawank22@users.noreply.github.com>
2023-07-16 17:22:08 +0530

    refactoring

--- app.js
+++ app.js
@@ -1,31 +1,42 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-	res.render('index')
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-server = app.listen(process.env.PORT || 3000)
+server = app.listen(process.env.PORT || 8080);
 
-const io = require("socket.io")(server)
+const io = require("socket.io")(server);
 
-io.on('connection', (socket) => {
-	console.log('New user connected')
-	socket.username = "Anonymous"
-    socket.on('change_username', (data) => {
-        socket.username = data.username
-    })
+io.on("connection", (socket) => {
+  socket.username = "Anonymous";
 
-    socket.on('new_message', (data) => {
-        io.sockets.emit('new_message', {message : data.message, username : socket.username});
-    })
-    socket.on('typing', (data) => {
-    	socket.broadcast.emit('typing', {username : socket.username})
-    })
-})
+  socket.on("change_username", (data) => {
+    socket.username = data.username;
+  });
 
+  socket.on("new_message", (data) => {
+    if (data.type === "text") {
+      io.sockets.emit("new_message", {
+        type: "text",
+        content: data.content,
+        username: socket.username,
+      });
+    } else if (data.type === "file") {
+      io.sockets.emit("new_message", {
+        type: "file",
+        content: data.content,
+        username: socket.username,
+      });
+    }
+  });
 
+  socket.on("typing", () => {
+    socket.broadcast.emit("typing", { username: socket.username });
+  });
+});
 
